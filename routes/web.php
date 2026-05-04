efbe0437db1dedfbc6719ad3d1b4418ebd970580
MASTANTUONO Ludo <ludo.mastantuono@GB42.local>
2026-05-04 10:28:06 +0200

    [UPDATE] route puzzles create

--- routes/web.php
+++ routes/web.php
@@ -26,28 +26,30 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+#pour le profil
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('puzzles', PuzzleController::class)->only(['index', 'show']);
+//Route::resource('puzzles', PuzzleController::class)->only(['index', 'show']);
+Route::resource('puzzles', PuzzleController::class);
 Route::resource('categories',CategorieController::class);
 
-
+#pour le panier
 Route::post('/panier/add/{id}', [PanierController::class, 'add'])->name('panier.add');
 Route::get('/panier', [PanierController::class, 'index'])->name('paniers.index');
 Route::post('/panier/remove/{id}', [PanierController::class, 'remove'])->name('panier.remove');
 Route::post('/panier/update/{id}', [PanierController::class, 'update'])->name('panier.update');
 
-// Checkout (auth obligatoire)
+#pour payer
 Route::middleware('auth')->group(function () {
-    Route::get('/checkout', [CheckoutController::class, 'show'])->name('checkout.show');              // page payer
+    Route::get('/checkout', [CheckoutController::class, 'show'])->name('checkout.show');
     Route::post('/checkout/address/update', [CheckoutController::class, 'updateAddress'])->name('checkout.address.update');
-    Route::post('/checkout/pay', [CheckoutController::class, 'pay'])->name('checkout.pay');          // clique sur "Valider la commande"
+    Route::post('/checkout/pay', [CheckoutController::class, 'pay'])->name('checkout.pay');
 
-    // PayPal (exemple de flux)
+    #pour paypal
     Route::get('/paypal/return', [CheckoutController::class, 'paypalReturn'])->name('paypal.return');
     Route::get('/paypal/cancel', [CheckoutController::class, 'paypalCancel'])->name('paypal.cancel');
 
